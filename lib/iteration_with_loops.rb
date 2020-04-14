680f42aa5a47be4504e1b8546bb42b3f666f39e8
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-14 18:51:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,11 +9,10 @@ def join_nested_strings(src)
     inner_count = 0
     while inner_count < src[count].length do
       if src[count][inner_count][0].is_a? String
-    
+        new_string = each.join(" ")
         inner_count += 1
       end
     end
-    new_string = each.join(" ")
     count += 1
   end
   new_string
