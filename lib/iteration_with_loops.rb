a58b17805cf93f879d6f746e9a620e80646a17bf
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-14 19:23:53 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
@@ -6,6 +8,7 @@ def join_nested_strings(src)
   sub_each = []
   new_string = ""
   while count < src.length do
+    binding.pry
     each << src[count]
     inner_count = 0
     while inner_count < src[count].length do
