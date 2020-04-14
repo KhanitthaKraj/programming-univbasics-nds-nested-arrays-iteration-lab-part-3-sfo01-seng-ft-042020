5e81725de2f88e30e20102778a41ded59a345c79
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-14 19:39:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
@@ -13,7 +11,6 @@ def join_nested_strings(src)
       end
       inner_count += 1
     end
-    binding.pry
     count += 1
   end
   new_string
